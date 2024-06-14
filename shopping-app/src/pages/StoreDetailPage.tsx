c5077192ec110613fed5fb3eb24aedc48344f8e5
nicolasrojas17 <narojasa@indracompany.com>
2024-06-14 06:45:33 -0500

    se actualiza archivo vercel.json

--- shopping-app/src/pages/StoreDetailPage.tsx
+++ shopping-app/src/pages/StoreDetailPage.tsx
@@ -118,7 +118,10 @@ const StoreDetailPage = () => {
           </Box>
         </Grid>
 
-        <Grid item xs={12} px={2} mt={5} mx={1}>
+<Typography variant="h4" mt={5} fontWeight={"bold"} textAlign="center">
+  Similar products
+</Typography>
+        <Grid item xs={12} px={2}  mx={1}>
           <Grid container spacing={2} mb={5} justifyContent={"center"}>
             {productsByCategory?.map((product: Product, index) => [
               <CardItem key={product.id} product={product} altImg={`card item-${index + 1}`} />,
